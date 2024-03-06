6f0a491b1054e0dfd9ead35451ad06c08123da2e
Danniel Nguyen <dannielnguyen289@gmail.com>
2024-03-07 00:08:12 +0700

    Initial project

--- src/main/java/com/stringeex/sample/scheduler/sample/SampleScheduler.java
+++ src/main/java/com/stringeex/sample/scheduler/sample/SampleScheduler.java
@@ -35,7 +35,7 @@ public class SampleScheduler {
     private Job sampleJob;
 
     @Async
-    @Scheduled(fixedRate = 1000)
+    @Scheduled(initialDelay = 1000, fixedDelay=Long.MAX_VALUE)
     public void consume() throws InterruptedException {
         // Get current java server date time
         Date currentDateTime = new Date();
